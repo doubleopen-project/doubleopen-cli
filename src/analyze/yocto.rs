886cb616886ee7436aa7b0d2a000ffedc243d201
Mikko Murto <mikko.murto@gmail.com>
2020-11-27 13:10:39 +0000

    add source archive finder for yocto

--- src/analyze/yocto.rs
+++ src/analyze/yocto.rs
@@ -1,5 +1,7 @@
 use crate::spdx::spdx::{Algorithm, FileInformation, PackageInformation, SPDX};
+use flate2::read::GzDecoder;
 use fs::File;
+use tar::Archive;
 use std::{
     collections::HashMap,
     fs,
@@ -76,14 +78,15 @@ pub fn get_list_of_packages_from_manifest<P: AsRef<Path>, R: AsRef<Path>>(
 }
 
 #[derive(Debug)]
-pub struct YoctoBuild {
+pub struct YoctoBuild<'yocto> {
     pub image_name: String,
     pub architecture: String,
     pub build_directory: PathBuf,
     pub manifest_entries: Vec<ManifestEntry>,
+    pub source_packages: Vec<YoctoSourcePackage<'yocto>>,
 }
 
-impl YoctoBuild {
+impl<'yocto> YoctoBuild<'yocto> {
     pub fn new<P: AsRef<Path>>(
         build_directory: P,
         manifest_file: P,
@@ -124,15 +127,28 @@ impl YoctoBuild {
             ..Default::default()
         })
     }
+
+    pub fn get_unique_reversed_packages(&self) -> Vec<&RuntimeReverse> {
+        let mut runtime_reverses: Vec<&RuntimeReverse> = self
+            .manifest_entries
+            .iter()
+            .map(|manifest_entry| &manifest_entry.runtime_reverse)
+            .collect();
+
+        runtime_reverses.sort_by_key(|&rr| &rr.package_name);
+        runtime_reverses.dedup();
+        runtime_reverses
+    }
 }
 
-impl Default for YoctoBuild {
+impl<'yocto> Default for YoctoBuild<'yocto> {
     fn default() -> Self {
         Self {
             architecture: "DEFAULT".into(),
             build_directory: "DEFAULT".into(),
             image_name: "DEFAULT".into(),
             manifest_entries: Vec::new(),
+            source_packages: Vec::new(),
         }
     }
 }
@@ -163,9 +179,7 @@ impl ManifestEntry {
             .ok_or_else(|| AnalyzerError::ParseError(line.into()))?
             .to_string();
 
-        let runtime_reverse_path = build_path
-            .as_ref()
-            .join(&package_name);
+        let runtime_reverse_path = build_path.as_ref().join(&package_name);
 
         let runtime_reverse = RuntimeReverse::new(runtime_reverse_path)?;
 
@@ -209,6 +223,74 @@ impl RuntimeReverse {
             version: package_version,
         })
     }
+
+    pub fn find_source_archive<P: AsRef<Path>>(
+        &self,
+        build_directory: P,
+    ) -> Result<PathBuf, AnalyzerError> {
+        let downloads_directory = build_directory.as_ref().join("downloads/");
+
+        let name_version = format!("{}-{}", &self.package_name, &self.version);
+
+        let mut downloads = downloads_directory.read_dir()?.map(|x| x.unwrap());
+
+        let archive = downloads.find(|download| {
+            download
+                .file_name()
+                .to_str()
+                .expect("Archive not found.")
+                .starts_with(&self.package_name)
+                && download
+                    .path()
+                    .extension()
+                    .expect("Archive not found.")
+                    .to_str()
+                    .expect("Archive not found.")
+                    != "done"
+        });
+
+        match archive {
+            Some(direntry) => Ok(direntry.path()),
+            None => Err(AnalyzerError::ParseError(
+                "Can't find source archive.".into(),
+            )),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct YoctoSourcePackage<'yocto> {
+    package_name: &'yocto str,
+    package_version: &'yocto str,
+    source_archive_path: &'yocto Path,
+    source_files: Vec<YoctoSourceFile<'yocto>>,
+}
+
+impl<'yocto> YoctoSourcePackage<'yocto> {
+    pub fn new(
+        package_name: &'yocto str,
+        package_version: &'yocto str,
+        source_archive_path: &'yocto Path,
+    ) -> Self {
+        // Create a temporary directory and unpack the archive there.
+        let temp_dir = tempfile::tempdir().unwrap();
+        let file = File::open(&source_archive_path).unwrap();
+        let tar = GzDecoder::new(file);
+        let mut archive = Archive::new(tar);
+        archive.unpack(&temp_dir.path()).unwrap();
+        Self {
+            package_name,
+            package_version,
+            source_archive_path,
+            source_files,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct YoctoSourceFile<'yocto> {
+    filename: &'yocto str,
+    sha256: String,
 }
 
 #[cfg(test)]
@@ -233,12 +315,48 @@ mod tests {
             .build_directory
             .ends_with("tests/examples/yocto/build"));
         assert_eq!(yocto.manifest_entries.len(), 5);
-        assert_eq!(yocto.manifest_entries[0].runtime_reverse.package_name, "adwaita-icon-theme".to_string());
-        assert_eq!(yocto.manifest_entries[0].runtime_reverse.version, "3.34.3".to_string());
-        assert_eq!(yocto.manifest_entries[1].runtime_reverse.package_name, "adwaita-icon-theme".to_string());
-        assert_eq!(yocto.manifest_entries[1].runtime_reverse.version, "3.34.3".to_string());
-        assert_eq!(yocto.manifest_entries[2].runtime_reverse.package_name, "alsa-utils".to_string());
-        assert_eq!(yocto.manifest_entries[2].runtime_reverse.version, "1.2.1".to_string());
+        assert_eq!(
+            yocto.manifest_entries[0].runtime_reverse.package_name,
+            "adwaita-icon-theme".to_string()
+        );
+        assert_eq!(
+            yocto.manifest_entries[0].runtime_reverse.version,
+            "3.34.3".to_string()
+        );
+        assert_eq!(
+            yocto.manifest_entries[1].runtime_reverse.package_name,
+            "adwaita-icon-theme".to_string()
+        );
+        assert_eq!(
+            yocto.manifest_entries[1].runtime_reverse.version,
+            "3.34.3".to_string()
+        );
+        assert_eq!(
+            yocto.manifest_entries[2].runtime_reverse.package_name,
+            "alsa-utils".to_string()
+        );
+        assert_eq!(
+            yocto.manifest_entries[2].runtime_reverse.version,
+            "1.2.1".to_string()
+        );
+    }
+
+    #[test]
+    fn find_source_archive() {
+        let mut build_directory = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        build_directory.push("tests/examples/yocto/build/");
+
+        let runtime_reverse = RuntimeReverse {
+            package_name: "alsa-utils".into(),
+            version: "1.2.1".into(),
+        };
+
+        let archive = runtime_reverse
+            .find_source_archive(&build_directory)
+            .unwrap();
+        let expected = build_directory.join("downloads/alsa-utils-1.2.1.tar.bz2");
+
+        assert_eq!(archive, expected);
     }
 
     #[test]
@@ -258,10 +376,14 @@ mod tests {
         test_manifest_path.push("tests/examples/yocto/build/tmp/deploy/images/qemux86-64/core-image-sato-qemux86-64.manifest");
 
         let mut runtime_reverse_dir_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-        runtime_reverse_dir_path.push("tests/examples/yocto/build/tmp/pkgdata/qemux86-64/runtime-reverse/");
+        runtime_reverse_dir_path
+            .push("tests/examples/yocto/build/tmp/pkgdata/qemux86-64/runtime-reverse/");
 
-        let packages =
-            super::get_list_of_packages_from_manifest(&test_manifest_path, &runtime_reverse_dir_path).unwrap();
+        let packages = super::get_list_of_packages_from_manifest(
+            &test_manifest_path,
+            &runtime_reverse_dir_path,
+        )
+        .unwrap();
 
         let expected_packages = vec![
             ManifestEntry {
@@ -270,8 +392,8 @@ mod tests {
                 version: "3.34.3".into(),
                 runtime_reverse: RuntimeReverse {
                     package_name: "adwaita-icon-theme".into(),
-                    version: "3.34.3".into()
-                }
+                    version: "3.34.3".into(),
+                },
             },
             ManifestEntry {
                 package_name: "adwaita-icon-theme-symbolic".into(),
@@ -279,8 +401,8 @@ mod tests {
                 version: "3.34.3".into(),
                 runtime_reverse: RuntimeReverse {
                     package_name: "adwaita-icon-theme".into(),
-                    version: "3.34.3".into()
-                }
+                    version: "3.34.3".into(),
+                },
             },
             ManifestEntry {
                 package_name: "alsa-utils-alsactl".into(),
@@ -288,8 +410,8 @@ mod tests {
                 version: "1.2.1".into(),
                 runtime_reverse: RuntimeReverse {
                     package_name: "alsa-utils".into(),
-                    version: "1.2.1".into()
-                }
+                    version: "1.2.1".into(),
+                },
             },
             ManifestEntry {
                 package_name: "alsa-utils-alsamixer".into(),
@@ -297,8 +419,8 @@ mod tests {
                 version: "1.2.1".into(),
                 runtime_reverse: RuntimeReverse {
                     package_name: "alsa-utils".into(),
-                    version: "1.2.1".into()
-                }
+                    version: "1.2.1".into(),
+                },
             },
             ManifestEntry {
                 package_name: "dbus-1".into(),
@@ -306,8 +428,8 @@ mod tests {
                 version: "1.12.16".into(),
                 runtime_reverse: RuntimeReverse {
                     package_name: "dbus".into(),
-                    version: "1.12.16".into()
-                }
+                    version: "1.12.16".into(),
+                },
             },
         ];
 
