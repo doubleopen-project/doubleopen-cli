df93aac4e42d63e2c1c46a4c17476be35a0f55cd
Mikko Murto <mikko.murto@hhpartners.fi>
2022-04-12 16:46:18 +0300

    refactor: move cli commands to separate module
    Signed-off-by: Mikko Murto <mikko.murto@hhpartners.fi>

--- src/doubleopen.rs
+++ src/doubleopen.rs
@@ -1,8 +1,16 @@
-use std::path::Path;
+use std::{path::Path, thread, time::Duration};
 
+use fossology_rs::{
+    job::{schedule_analysis, ScheduleAgents},
+    upload::{filesearch, get_upload_by_id, new_upload_from_file, Hash},
+    Fossology, FossologyError,
+};
+use log::info;
 use regex::Regex;
 use spdx_rs::models::{PackageInformation, SpdxExpression};
 use spdx_toolkit::license_list::LicenseList;
+
+use crate::utilities::hash256_for_path;
 
 // SPDX-FileCopyrightText: 2020 HH Partners
 //
@@ -586,4 +594,104 @@ mod test_super {
         assert!(skip_package_upload(&tzdata_path, &closed_packages));
         assert!(skip_package_upload(&git_package_path, &closed_packages));
     }
+}
+
+pub fn upload_missing_archives_to_fossology<P: AsRef<Path>>(
+    source_archives: Vec<P>,
+    fossology: &Fossology,
+    fossolody_folder: &i32,
+    spdx_packages: &[PackageInformation],
+    dry_run: bool,
+) -> Result<(), FossologyError> {
+    info!("Uploading missing archives to Fossology.",);
+
+    let packages_to_skip = get_packages_with_closed_license(spdx_packages);
+
+    let (paths_to_skip, paths_to_upload): (Vec<_>, Vec<_>) = source_archives
+        .iter()
+        .partition(|archive| skip_package_upload(archive, &packages_to_skip));
+
+    if !dry_run {
+        for path in paths_to_skip {
+            let display = path.as_ref().display();
+            info!("Will not upload {} based on its license.", display);
+        }
+
+        for file in paths_to_upload {
+            let sha256 = hash256_for_path(&file);
+            let input = vec![Hash::from_sha256(&sha256)];
+
+            let not_on_fossology = filesearch(fossology, &input, None).unwrap().is_empty();
+
+            if not_on_fossology {
+                let upload = new_upload_from_file(fossology, *fossolody_folder, &file)?;
+
+                let ununpack_in_progress = |fossology, upload_id| -> Result<bool, FossologyError> {
+                    get_upload_by_id(fossology, upload_id).map_or_else(
+                        |err| match err {
+                            FossologyError::Other(error_string) => {
+                                if error_string.contains("Ununpack job not started") {
+                                    Ok(true)
+                                } else {
+                                    Err(FossologyError::Other(error_string))
+                                }
+                            }
+                            err => Err(err),
+                        },
+                        |res| Ok(res.is_none()),
+                    )
+                };
+
+                while ununpack_in_progress(fossology, upload.upload_id)? {
+                    info!(
+                        "Waiting for {} to be unarchived on Fossology.",
+                        &file.as_ref().display()
+                    );
+                    thread::sleep(Duration::from_secs(10));
+                }
+
+                let mut analysis_input = ScheduleAgents::default();
+                analysis_input.analysis.bucket = true;
+                analysis_input.analysis.copyright_email_author = true;
+                analysis_input.analysis.ecc = true;
+                analysis_input.analysis.keyword = true;
+                analysis_input.analysis.mime = true;
+                analysis_input.analysis.monk = true;
+                analysis_input.analysis.nomos = true;
+                analysis_input.analysis.ojo = true;
+                analysis_input.analysis.package = true;
+
+                analysis_input.decider.new_scanner = true;
+                analysis_input.decider.nomos_monk = true;
+                analysis_input.decider.ojo_decider = true;
+
+                schedule_analysis(
+                    fossology,
+                    *fossolody_folder,
+                    upload.upload_id,
+                    None,
+                    &analysis_input,
+                )
+                .unwrap();
+            } else {
+                info!(
+                    "{} exists on Fossology, did not upload again.",
+                    &file.as_ref().display()
+                );
+            }
+        }
+    } else {
+        info!("DRY_RUN: Real run would UPLOAD the following packages:");
+        for path in paths_to_upload {
+            let display = path.as_ref().display();
+            info!("UPLOAD: {}", display);
+        }
+
+        info!("DRY_RUN: Real run would SKIP the following packages:");
+        for path in paths_to_skip {
+            let display = path.as_ref().display();
+            info!("SKIP: {}", display);
+        }
+    }
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -4,87 +4,9 @@
 
 //! Double Open Command Line Utility.
 
-use clap::{Parser, ValueHint};
-use doubleopen_cli::{
-    commands::upload_missing_archives_to_fossology, populate_spdx_document_from_fossology,
-};
+use clap::StructOpt;
+use doubleopen_cli::commands::{main_command, Opts};
 use env_logger::Env;
-use fossology_rs::Fossology;
-use spdx_rs::models::SPDX;
-use spdx_toolkit::license_list::LicenseList;
-use std::{
-    fs::{self, read_to_string},
-    path::PathBuf,
-};
-
-/// Command line options.
-#[derive(Parser, Debug)]
-#[clap(author, about, version)]
-struct Opts {
-    /// Subcommand to run.
-    #[clap(subcommand)]
-    subcmd: SubCommand,
-}
-
-/// Interact with Fossology and process SPDX document with the data.
-#[derive(Parser, Debug)]
-enum SubCommand {
-    /// Interact with Fossology.
-    #[clap(author, version)]
-    Fossology(FossologyArguments),
-}
-
-/// Arguments for the Fossology subcommand.
-#[derive(Parser, Debug)]
-struct FossologyArguments {
-    /// URL of the Fossology instance to use.
-    /// Example: `http://localhost/repo/api/v1`.
-    #[clap(short, long, value_hint = ValueHint::Url)]
-    uri: String,
-
-    /// Access token for the Fossology instance.
-    #[clap(short, long)]
-    token: String,
-
-    /// Action to do with Fossology.
-    #[clap(subcommand)]
-    action: FossologyAction,
-}
-
-/// Sub(sub)commands for the Fossology subcommands.
-#[derive(Parser, Debug)]
-enum FossologyAction {
-    /// Upload source archives to Fossology.
-    Upload {
-        /// Source archives to upload to Fossology. Use pattern matching to upload multiple files.
-        source_archive_paths: Vec<PathBuf>,
-
-        /// ID of the folder in Fossology to upload the source to.
-        #[clap(short, long)]
-        folder: i32,
-
-        /// Path to the SPDX Document to get the packages that should be skipped. Packages with
-        /// CLOSED in their declared license won't be uploaded.
-        #[clap(long, parse(from_os_str), value_hint = ValueHint::FilePath)]
-        spdx: PathBuf,
-
-        /// Don't actually upload the packages, but print packages that would be uploaded and
-        /// packages that would be skipped based on having a closed license.
-        #[clap(long)]
-        dry_run: bool,
-    },
-
-    /// Populate an SPDX file with license and copyritght information from Fossology.
-    Query {
-        /// Path to the input SPDX.
-        #[clap(short, long, parse(from_os_str), value_hint = ValueHint::FilePath)]
-        input: PathBuf,
-
-        /// Path to output the populated SPDX document to.
-        #[clap(short, long, parse(from_os_str), value_hint = ValueHint::FilePath)]
-        output: PathBuf,
-    },
-}
 
 fn main() -> anyhow::Result<()> {
     // Initialize logging.
@@ -95,42 +17,7 @@ fn main() -> anyhow::Result<()> {
     // Get the command line arguments.
     let opts: Opts = Opts::parse();
 
-    // Process subcommands.
-    match opts.subcmd {
-        // Process Fossology subcommand.
-        SubCommand::Fossology(fossology_arguments) => {
-            let fossology = Fossology::new(&fossology_arguments.uri, &fossology_arguments.token)?;
+    main_command(opts)?;
 
-            match fossology_arguments.action {
-                // Process upload subcommand of Fossology.
-                FossologyAction::Upload {
-                    source_archive_paths,
-                    folder,
-                    spdx,
-                    dry_run,
-                } => {
-                    let file_contents = read_to_string(&spdx)?;
-                    let spdx: SPDX = serde_json::from_str(&file_contents)?;
-                    upload_missing_archives_to_fossology(
-                        source_archive_paths,
-                        &fossology,
-                        &folder,
-                        &spdx.package_information,
-                        dry_run,
-                    )?;
-                }
-
-                // Process query subcommand of Fossology.
-                FossologyAction::Query { input, output } => {
-                    let file_contents = read_to_string(&input)?;
-                    let mut spdx: SPDX = serde_json::from_str(&file_contents)?;
-                    let license_list = LicenseList::from_github()?;
-                    populate_spdx_document_from_fossology(&fossology, &mut spdx, &license_list)?;
-                    let json_string = serde_json::to_string_pretty(&spdx)?;
-                    fs::write(&output, json_string)?;
-                }
-            }
-        }
-    }
     Ok(())
 }
