82432d861c6b3fa263918790eafa18b8f6a9b8b9
Mikko Murto <mikko.murto@hhpartners.fi>
2022-04-13 15:27:27 +0300

    refactor: move spdx license validation to separate function
    Signed-off-by: Mikko Murto <mikko.murto@hhpartners.fi>

--- src/fossology/convert_licenses.rs
+++ src/fossology/convert_licenses.rs
@@ -2,9 +2,10 @@
 //
 // SPDX-License-Identifier: MIT
 
+use spdx_rs::models::SpdxExpression;
 use spdx_toolkit::license_list::LicenseList;
 
-use super::doubleopen_licenses::gpl_or_later_conversion;
+use super::doubleopen_licenses::{gpl_or_later_conversion, is_do_license};
 
 /// Convert scanner hits from Fossology to vec of SPDX expressions.
 pub fn license_information_to_spdx_expressions(
@@ -39,4 +40,59 @@ pub fn license_information_to_spdx_expressions(
 /// Sanitize string to conform to SPDX license expression spec.
 fn sanitize_spdx_expression(lic: String) -> String {
     lic.replace(&['(', ')', '[', ']'][..], "")
+}
+
+/// Update [`SpdxExpression`] to be valid SPDX license.
+pub fn update_license_to_valid_spdx(
+    license: &SpdxExpression,
+    license_list: &LicenseList,
+) -> anyhow::Result<SpdxExpression> {
+    let mut lic = license.to_string();
+    let identifiers = license.identifiers();
+
+    for identifier in identifiers {
+        if license_list.includes_license(&identifier.replace('+', ""))
+            || license_list.includes_exception(&identifier)
+            || is_do_license(&identifier)
+            || identifier.starts_with("LicenseRef-")
+            || identifier == "Dual-license"
+            || identifier == "NOASSERTION"
+            || identifier == "NONE"
+        {
+            continue;
+        } else {
+            lic = lic.replace(&identifier, &format!("LicenseRef-{}", identifier));
+        }
+    }
+
+    Ok(SpdxExpression::parse(&lic)?)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn update_licenseref_to_valid_spdx() {
+        let original = SpdxExpression::parse("CLOSED").unwrap();
+
+        let license_list = LicenseList::from_github().unwrap();
+        let actual = update_license_to_valid_spdx(&original, &license_list).unwrap();
+
+        let expected = SpdxExpression::parse("LicenseRef-CLOSED").unwrap();
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn do_not_update_valid_spdx() {
+        let original = SpdxExpression::parse("MIT").unwrap();
+
+        let license_list = LicenseList::from_github().unwrap();
+        let actual = update_license_to_valid_spdx(&original, &license_list).unwrap();
+
+        let expected = SpdxExpression::parse("MIT").unwrap();
+
+        assert_eq!(actual, expected);
+    }
 }
